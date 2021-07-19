e7ffbc4fe9170f673bb2feeea2137d68c35fd8b6
Harshit <harshit16sahu@gmail.com>
2021-07-19 10:45:48 +0530

    Context Menu CTA button to perform onClick events

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,51 @@
 import { NewsArticle } from "../utils/Fetch";
 import styled from "styled-components";
+import { Menu, Dropdown, Button } from "antd";
 
 export function Card(article: NewsArticle) {
+  /**
+   * These are menu dropdown values.
+   */
+  const menu = (
+    <Menu>
+      <Menu.Item
+        onClick={() => alert(" ID : " + article.id + "\n Action: Edit")}
+      >
+        Edit
+      </Menu.Item>
+      <Menu.Item
+        onClick={() => alert(" ID : " + article.id + "\n Action: Delete")}
+      >
+        Delete
+      </Menu.Item>
+    </Menu>
+  );
   return (
     <>
       <CardContainer>
         <ImageContainer src={article.thumbnail} alt="none"></ImageContainer>
-        <Title>{article.title}</Title>
-        <Description>{article.description}</Description>
+        <MetaContainer>
+          <div>
+            <Title>{article.title}</Title>
+            <Description>{article.description}</Description>
+          </div>
+          <AntdDropDown overlay={menu} placement="topLeft" arrow>
+            <Button>:</Button>
+          </AntdDropDown>
+        </MetaContainer>
       </CardContainer>
     </>
   );
 }
+
+const MetaContainer = styled.div`
+  display: flex;
+`;
+
+const AntdDropDown = styled(Dropdown)`
+  position: relative;
+  top: 1.5rem;
+`;
 
 const CardContainer = styled.div`
   height: 300px;
